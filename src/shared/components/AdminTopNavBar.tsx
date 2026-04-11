65eaaca6e1920414da0bb612a93960bf7d10642f
ttoddo <hramchencov@gmail.com>
2026-04-11 23:54:17 +0300

    оформление админки

--- src/shared/components/AdminTopNavBar.tsx
+++ src/shared/components/AdminTopNavBar.tsx
@@ -11,6 +11,7 @@ import Button from "./Button";
 
 export default function AdminTopNavBar(){
    const [isOpen, setIsOpen] = useState<boolean>(false)
+   const [file, setFile] = useState<Blob | null>(null)
     const openKeyCreateModal = () => {
         setIsOpen(true)
     }
@@ -18,17 +19,32 @@ export default function AdminTopNavBar(){
     const closeKeyCreateModal = () => {
         setIsOpen(false);
     }
+
+    const handleChange = async (e: React.ChangeEvent<HTMLInputElement>) => {
+        if (e.target.files && e.target.files.length > 0) {
+            setFile(e.target.files[0])
+            console.log(file)
+        }
+        
+            
+
+    }
+
     return (
-        <div className="h-25 ml-33.5 mt-13.75 flex justify-between pl-3.75  items-end">
-            <div className="flex gap-6.25 items-end w-180 rounded-lg bg-bgModal dark:bg-bgModalD rounded-lgw relative">
+        <div className="h-25 ml-33.5 mt-13.75 flex flex-col justify-between pl-3.75  items-start">
+            <div className="flex gap-6.25 items-end w-full rounded-lg bg-bgModal dark:bg-bgModalD rounded-lgw relative">
                 <input type="url" placeholder="Ссылка на календарный учебный график" className="w-[78%] rounded-lg text-tDark outline-0 dark:text-tDarkD p-3"></input>
                 <Button children="Сохранить" className="absolute right-1 bottom-1" />
             </div> 
-            <div className="flex gap-6.25">
+            <div className="w-full flex  gap-6.25">
                 <Button onClick={openKeyCreateModal} children="Создать ключ"/>
                 <Button children="Миграция"/>
-                <Button children="Список Студентов"/>
-                <Button children="Очистка БД"/>
+                <Button children="Загрузить cтудентов"/>
+                <label htmlFor="adminInput" className="cursor-pointer  p-2 bg-primaryD text-tLight dark:text-tLightD rounded-lg font-medium ">
+                        Загрузить студентов .clsx
+                </label>
+                <input type="file" id="adminInput" name="fileUpload" onChange={() => handleChange} placeholder="Загрузить студентов .clsx"  className="hidden"/>
+                
             </div>
             <KeyCreateModal isOpen={isOpen} close={closeKeyCreateModal  } />
        </div> 
