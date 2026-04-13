84d6b537a9c72e3ad6bf85f2f5f8683bfdf409ce
D00RF1X3R <pismomaxaf@gmail.com>
2026-04-13 09:19:15 +0300

    --

--- src/shared/modals/RoutesModal.tsx
+++ src/shared/modals/RoutesModal.tsx
@@ -1,4 +1,4 @@
-import { Field, Input, Label } from "@headlessui/react";
+
 
 
 
@@ -7,9 +7,6 @@ export default function ModalInput() {
 
 
     return (
-        <Field className="flex flex-col gap-1 text-tLight dark:text-tLightD">
-            <Label>{children}</Label>
-            <Input type={type} placeholder={placeholder} onChange={onChange} value={value} className="bg-bgLight dark:bg-bgLightD rounded-sm h-10 data-focus:outline-bgLightD data-focus:outline-2"/>
-        </Field>
+        <></>
     )
 }
